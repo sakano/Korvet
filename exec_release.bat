d541d250179371309d889dfd80907917aa11ba84
Komatsu Naoya <komatsu@biscrat.com>
2015-04-16 08:42:15 +0900

    終了時のエラーを抑制

--- exec_release.bat
+++ exec_release.bat
@@ -6,4 +6,4 @@ taskkill /f /im "tvpwin32_dbg.exe" >NUL 2>&1
 
 call compile_release.bat
 
-start "" tvpwin32_p.exe "%~dp0start" -rlmode="yes" -forcelog="yes"
+start "" tvpwin32_p.exe "%~dp0start" -rlmode="yes"
