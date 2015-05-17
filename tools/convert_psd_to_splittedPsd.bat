74483ef082420eb532c5ca4cd7aed0a41689ee8c
Komatsu Naoya <komatsu@biscrat.com>
2015-05-17 19:04:10 +0900

    立ち絵表示を実装

---
+++ tools/convert_psd_to_splittedPsd.bat
@@ -0,0 +1,2 @@
+cd ../
+tvpwin32.exe "%~dp0/" -startup="convert_psd_to_splittedPsd.tjs" -debug="yes" -debugwin="yes" -warnrundelobj="no"
---
+++ tools/convert_splittedPsd_to_layered.bat
@@ -0,0 +1,2 @@
+cd ../
+tvpwin32.exe "%~dp0/" -startup="convert_splittedPsd_to_layered.tjs" -debug="yes" -debugwin="yes" -warnrundelobj="no"
